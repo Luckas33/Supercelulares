67886cf9fc20d48a83e279d97eb358e9a89c0d3e
Luckas33~ <sobrallucas33@gmail.com>
2023-06-24 20:49:39 -0300

    colocando a key

--- components/prod/produtoss.js
+++ components/prod/produtoss.js
@@ -16,16 +16,21 @@ export default async function Produtoss(){
         <br></br>
         <br></br>
         <br></br>
-        
+        <br></br>
             <div className={styles.prod0}>
-            <h1>Produtos</h1>
+                <h1>Produtos</h1>
             </div>
            <div classsName={styles.prod1}>
-            <Image src={posts.img} alt="foto celulares" width={100} height={100}></Image>
-            {posts.map((post) =>{
-               return <p>{post.nome}{post.preco}{post.descricao}</p>     
+            
+            {posts.map((post,index) =>{
+               return <p key={index} className={styles.prod2}>{post.nome}<Image src={post.img} alt="foto celulares" width={100} height={100}></Image><p className={styles.prod3}>{post.preco}{post.descricao}</p></p>
+                
+                
+                   
             }) }
-            </div>
+
+           
+        </div>
         </div>
     )
 }
--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains: ["apitrainees.herokuapp.com"]
+        domains: [
+        "w7.pngwing.com",
+        "storage.googleapis.com",
+        "files.tecnoblog.net",
+        "oasis.opstatics.com",
+        "oasis.opstatics.com",
+        "fscl01.fonpit.de",
+        "t.ctcdn.com.br"
+        ]
     }
 }
 
 module.exports = nextConfig
+
+// ["files.tecnoblog.net"] ["oasis.opstatics.com"] ["oasis.opstatics.com"] ["fscl01.fonpit.de"] ["t.ctcdn.com.br"]
